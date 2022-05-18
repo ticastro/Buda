c2c2efdd31cd0c64ede3274edd03d59ba2c4cb7d
ticastro <ticastro@uc.cl>
2022-05-18 10:49:57 -0400

    :sparkles: final message handle

--- src/main/main.py
+++ src/main/main.py
@@ -11,7 +11,11 @@ def main():
   while initialStation == endStation:
     endStation = stationSelector(3, network)
   color = colorSelector()
-  network.BFS(initialStation, endStation, color)
+  network.bfs(initialStation, endStation, color)
+  if network.allStations[endStation].previousStations == []:
+    print(f'No se puede llegar a {endStation}')
+  else:
+    print(f'Las estaciones previas de {endStation} son {network.allStations[endStation].previousStations} ')
 
 if __name__ == '__main__':
   main()
